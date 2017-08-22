b4bbd112b5d98e5103dfc2f9067bf59312277276
Kei Tashima <tassi.yuzukko@gmail.com>
2017-08-22 22:05:49 +0900

    Two-Phase Terminationパターンを追加

---
+++ _10_TwoPhaseTermination/CountupThread.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _10_TwoPhaseTermination
+{
+    class CountupThread
+    {
+        long _counter = 0;
+        volatile bool _shutdownRequested = false;
+
+        public void ShutdownRequest()
+        {
+            _shutdownRequested = true;
+        }
+
+        public bool IsShutdownRequested()
+        {
+            return _shutdownRequested;
+        }
+
+        public Task Task()
+        {
+            return new Task(()=>{
+                try
+                {
+                    while (!IsShutdownRequested())
+                    {
+                        // 作業中
+                        DoWork();
+                    }
+                }
+                finally
+                {
+                    // 終了処理中
+                    DoShutdown();
+                }
+            });
+        }
+
+        void DoWork()
+        {
+            _counter++;
+            Console.WriteLine("DoWork: counter = " + _counter);
+            Thread.Sleep(500);
+        }
+
+        void DoShutdown()
+        {
+            Console.WriteLine("DoShutdown: counter = ", _counter);
+        }
+
+
+    }
+}
---
+++ _10_TwoPhaseTermination/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace _10_TwoPhaseTermination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("main: BEGIN");
+
+            var t = new CountupThread();
+            var task = t.Task();
+            task.Start();
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine("main: ShutdownRequest");
+            t.ShutdownRequest();
+
+            Console.WriteLine("main; join");
+            task.Wait();
+
+            Console.WriteLine("main: END");
+            Console.ReadKey();
+        }
+    }
+}
